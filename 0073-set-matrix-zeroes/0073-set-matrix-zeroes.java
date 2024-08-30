f5af5edd22a202f2832ebf00bec89b1674eb2151
Alok Gupta <138797208+xevohere@users.noreply.github.com>
2024-08-30 21:40:38 +0530

    Time: 1 ms (79.15%), Space: 45.7 MB (17.92%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,22 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         
-        //int[][] arr = matrix.clone();
-        int[][] arr = new int[matrix.length][matrix[0].length];
-        
+        int row[] = new int[matrix.length];
+        int column[] = new int[matrix[0].length];
+
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
-                arr[i][j]=matrix[i][j];
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    column[j]=1;
+                }
             }
         }
-            
+
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
-                if(arr[i][j]==0){
-                    for(int p=0; p<matrix.length; p++){
-                        matrix[p][j]=0;
-                    }
-                    for(int q=0; q<matrix[0].length; q++){
-                        matrix[i][q]=0;
-                    }
+                if(row[i]==1||column[j]==1){
+                    matrix[i][j]=0;
                 }
             }
         }
