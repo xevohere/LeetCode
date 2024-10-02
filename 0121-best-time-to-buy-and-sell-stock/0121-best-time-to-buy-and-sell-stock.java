f1e8be39fc181fc0745c953f97d3bf21e7280a55
Alok Gupta <138797208+xevohere@users.noreply.github.com>
2024-10-02 23:30:40 +0530

    Time: 2 ms (74.43%), Space: 61.4 MB (81.18%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,31 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int max = 0;
+//         int profit = 0;
+//         int j=1, i=0;
+//         while(j<prices.length){
+//                 if(prices[i]<prices[j]){
+//                     profit = prices[j]-prices[i];
+//                     max = max>profit?max:profit;
+//                     j++;
+//                 }
+//                 else{
+//                     i++;
+//                     j=i+1;
+//                 }
+//         }
+//     return max;
+//     }
+// }
 class Solution {
     public int maxProfit(int[] prices) {
-        int max = 0;
-        int profit = 0;
-        int j=1, i=0;
-        while(j<prices.length){
-                if(prices[i]<prices[j]){
-                    profit = prices[j]-prices[i];
-                    max = max>profit?max:profit;
-                    j++;
-                }
-                else{
-                    i++;
-                    j=i+1;
-                }
+        int max_profit=0;
+        int min_price=Integer.MAX_VALUE;
+
+        for(int i=0;i<prices.length;i++){
+            min_price=Math.min(min_price,prices[i]);
+            max_profit=Math.max(max_profit,prices[i]-min_price);
         }
-    return max;
+        return max_profit;
     }
 }
