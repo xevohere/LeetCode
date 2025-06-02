85976571bb504c7a1c3e2d650747d02f62af7de0
Alok Gupta <138797208+xevohere@users.noreply.github.com>
2025-06-02 08:01:06 +0530

    Time: 3 ms (22.28%), Space: 18 MB (10.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,10 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-    
-        dp = [1] * n  # Initialize first row with 1s
-        for i in range(1, m):
-            new_row = [1]  # First column has 1 way
-            for j in range(1, n):
-                new_row.append(dp[j] + new_row[j-1])
-            dp = new_row
-        return dp[n-1]
+        memo = {}
+        def dp(i:int, j:int)->int:
+            if i==m-1 and j==n-1:
+                return 1
+            if i>=m or j>=n:
+                return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            else:
+                memo[(i,j)] = dp(i+1,j) + dp(i,j+1)
+                return memo[(i,j)]
+        return dp(0,0)
