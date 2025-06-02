bf8c95fb0d6d1ac8ae28abb23cbb5555cff3904e
Alok Gupta <138797208+xevohere@users.noreply.github.com>
2025-06-02 08:14:55 +0530

    Time: 0 ms (100%), Space: 17.9 MB (45%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,14 +1,4 @@
+from math import comb
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
-        def dp(i:int, j:int)->int:
-            if i==m-1 and j==n-1:
-                return 1
-            if i>=m or j>=n:
-                return 0
-            if (i,j) in memo:
-                return memo[(i,j)]
-            else:
-                memo[(i,j)] = dp(i+1,j) + dp(i,j+1)
-                return memo[(i,j)]
-        return dp(0,0)
+        return comb(m+n-2, min(m-1, n-1))
